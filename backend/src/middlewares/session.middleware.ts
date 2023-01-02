45d3bcf6db3ab4549a2155715b0db10a543eb174
joalisonpereira <joalison.pereira@hotmail.com>
2023-01-02 09:46:08 -0300

    try docker config

--- backend/src/middlewares/session.middleware.ts
+++ backend/src/middlewares/session.middleware.ts
@@ -1,22 +1,28 @@
-import session from "express-session";
+import session, { Store } from "express-session";
 import connectRedis from "connect-redis";
 import { createClient } from "redis";
 import { Application } from "express";
 import Env from "src/common/env";
 
-const RedisStore = connectRedis(session);
+let store: Store | undefined;
 
-const redisClient = createClient({
-  legacyMode: true,
-  url: Env.get("REDIS_URL", ""),
-});
+if (Env.get("NODE_ENV") === "production") {
+  const RedisStore = connectRedis(session);
 
-redisClient.connect().catch(console.error);
+  const redisClient = createClient({
+    legacyMode: true,
+    url: Env.get("REDIS_URL", ""),
+  });
+
+  redisClient.connect().catch(console.error);
+
+  store = new RedisStore({ client: redisClient });
+}
 
 export default (app: Application) =>
   app.use(
     session({
-      store: new RedisStore({ client: redisClient }),
+      store,
       saveUninitialized: true,
       secret: Env.get("SESSION_KEY", "1234"),
       resave: false,
